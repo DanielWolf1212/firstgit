719733d6a78f46d72d727f93215598cd96abdb56
Daniel <dukedaniel1212@gmail.com>
2021-08-11 12:24:35 +0530

    Master Commit

---
+++ src/main/java/com/FirstGit/controller/Controllers.java
@@ -0,0 +1,13 @@
+package com.FirstGit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controllers {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Daniel";
+	}
+}
